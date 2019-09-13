c8c3f4d050aa0c55865415de7372ba72f54f8e8e
luoxue <luoxue@zhuanzhuan.com>
2019-09-13 18:16:29 +0800

    fix

--- copy-dist.js
+++ copy-dist.js
@@ -9,27 +9,8 @@ class CopyDirWebpackPlugin {
   }
   apply(compiler) {
     compiler.plugin('done', (stats) => {
-      (async function() {
-        execa.shellSync('cp -r dist ../dist')
-        execa.shellSync('cd ../dist')
-        execa.shellSync('git init')
-        try {
-          execa.shellSync('git remote rename origin old-origin')
-        } catch (error) {
-          
-        }
-        try {
-          execa.shellSync('git remote add origin https://github.com/luoxue-victor/luoxue.github.io.git')
-        } catch (error) {
-
-        }
-        execa.shellSync('git add .')
-        execa.shellSync('git commit -m "fix"')
-        execa.shellSync('git pull origin master --allow-unrelated-histories')
-        execa.shellSync('git push origin master')
-
-        console.log('完成')
-      })()
+      execa.shellSync('cp -r dist ../dist')
+      console.log('完成')
     });
   }
 }
