24c5e9b647f7bb15d51362f0b09e2edb0578b333
luoxue <luoxue@zhuanzhuan.com>
2019-10-12 23:56:30 +0800

    添加了工程化的目录

--- docs-dist.js
+++ docs-dist.js
@@ -1,4 +1,3 @@
-const execa = require('execa')
 const fs = require('fs-extra')
 const globby = require('globby')
 const path = require('path')
@@ -7,6 +6,9 @@ let readmeContent = `# 技匠
 > 总结了个人的一些新技术、最佳实践、工程化、效率、工作体会等文章。欢迎star、pr，微信luoxue2479，下面是我的公众号，可以关注我【前端技匠】，随时获取最新动态哦！
 
 ![前端技匠](public/gongzhonghao.jpeg)
+
+执行 npm run dev 可以运行结果调试
+执行 npm run build 构建工程化
 `;
 class CopyDirWebpackPlugin {
   constructor(options) {
